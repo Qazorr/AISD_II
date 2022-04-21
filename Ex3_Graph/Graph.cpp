c9b361b8547e637b11fe87151eed0ecd664b3b14
Qazorr <kpiatkowski223@gmail.com>
2022-04-21 12:06:50 +0200

    Added chosing the type of graph

--- Ex3_Graph/Graph.cpp
+++ Ex3_Graph/Graph.cpp
@@ -1,9 +1,30 @@
 #include "Graph.hpp"
+#include <string_view>
 
-int main()
+int main(int argc, char* argv[])
 {
-    GraphAsMatrix g(10, true);
-    std::cout << "--------------------\n";
+    if(argc != 2) {
+        std::cerr << "Wykonaj program z flaga: -undir albo -dir\n";
+        exit(EXIT_FAILURE);
+    }
+
+    bool dir;
+    if(std::string_view(argv[1]) == "-undir") {
+        dir = false;
+        std::cout << "\033[1;32m=Wybrano nieskierowany graf=\033[0m\n"; 
+    }
+    else if(std::string_view(argv[1]) == "-dir") {
+        dir = true;
+        std::cout << "\033[1;32m==Wybrano skierowany graf==\033[0m\n"; 
+    } 
+    else {
+        std::cerr << "Wykonaj program z flaga: -undir albo -dir";
+        exit(EXIT_FAILURE);
+    }    
+
+    std::cout <<  "\033[1;32m-------Pierwsza część-------\033[0m" << std::endl;
+
+    GraphAsMatrix g(10, dir);
     std::cout << "Number of Verticies: " << g.NumberOfVertices() << std::endl;
     std::cout << "Number of Edges: " << g.NumberOfEdges() << std::endl;
     std::cout << "--------------------\n";
@@ -64,5 +85,28 @@ int main()
 
     e->weight = e->V0()->Number() + e->V1()->Number();
     std::cout << e->weight << std::endl;
+
+    std::cout <<  "\033[1;32m-------Druga część-------\033[0m" << std::endl;
+
+    g.print_verticies();
+ 
     std::cout << "--------------------\n";
+
+    g.AddEdge(3,5);
+    g.AddEdge(8,3);
+    g.AddEdge(5,2);
+
+    g.print_edges();
+ 
+    std::cout << "--------------------\n";
+
+    g.print_em_edges(3);
+ 
+    std::cout << "--------------------\n";
+
+    g.print_in_edges(2);
+ 
+    std::cout << "--------------------\n";
+
+    exit(EXIT_SUCCESS);
 }
